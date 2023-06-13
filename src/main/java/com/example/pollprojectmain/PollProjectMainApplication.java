fefd2bd1f27eb7edb4239699caf2cf016e3bfe4f
komead <110348640+komead@users.noreply.github.com>
2023-06-13 23:06:59 +0300

    #сreate a project structure

---
+++ src/main/java/com/example/pollprojectmain/PollProjectMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.pollprojectmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollProjectMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollProjectMainApplication.class, args);
+	}
+
+}
