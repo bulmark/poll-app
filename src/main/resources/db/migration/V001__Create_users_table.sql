f1e96f8a691a8bf299d637059c7607a376b84761
srrlds <shamahamaa@gmail.com>
2023-06-14 19:20:30 +0300

    added migration file with user table creation

---
+++ src/main/resources/db/migration/V001__Create_users_table.sql
@@ -0,0 +1,9 @@
+create table users
+(
+    id serial not null,
+    email varchar(50) unique,
+    username varchar(50) unique,
+    password varchar(64),
+    role varchar(255) check (role in ('USER','MODERATOR','ADMIN')),
+    primary key (id)
+);
