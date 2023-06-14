44534bf91c9c11580336fd9c8b71082760d3f4b2
Степанюк Антон <shamahamaa@gmail.com>
2023-06-14 19:50:01 +0300

    add polls table migration file

---
+++ src/main/resources/db/migration/V003_Create_polls_table.sql
@@ -0,0 +1,10 @@
+create table polls
+(
+    create_at date not null,
+    id serial not null,
+    owner_id integer not null,
+    period interval,
+    up_to_date date not null,
+    text varchar(255) not null,
+    primary key (id)
+);
