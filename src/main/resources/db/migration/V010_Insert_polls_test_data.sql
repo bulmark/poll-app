d50c55f9b9b3f645116af2ca7b809bdf0f455312
Степанюк Антон <shamahamaa@gmail.com>
2023-06-17 13:05:29 +0300

    14 add polls test data

---
+++ src/main/resources/db/migration/V010_Insert_polls_test_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO polls(owner_id, text)
+VALUES (2, 'Удобства использования микроволновки');
+
+INSERT INTO polls(owner_id, period, text, voting_time)
+VALUES (2, '30 days', 'Ваше мнение о садоводстве', '12 hours');
+
+INSERT INTO polls(owner_id, period, up_to_date, text, voting_time)
+VALUES (3, '30 days', '2023-12-31', 'Влияние употребления фасоли на ваш организм', '12 hours');
+
+INSERT INTO polls(owner_id, text, voting_time)
+VALUES (3, 'Ваши знания о трансмутации', '1 hours');
