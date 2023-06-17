c156efad4ed0b518b6ad9152e523795e36c66ff9
Степанюк Антон <shamahamaa@gmail.com>
2023-06-17 11:14:23 +0300

    #13 add migration file with insertion users test data

---
+++ src/main/resources/db/migration/V008_Insert_users_test_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO users(email, username, password, role)
+VALUES
+    ('admin@example.com', 'admin', 'admin', 'ADMIN'),
+    ('moder1@example.com', 'moder1', 'moder', 'MODERATOR'),
+    ('moder2@example.com', 'moder2', 'moder', 'MODERATOR'),
+    ('raul.esteban@example.com', 'goldenswan706', 'baltimor', 'USER'),
+    ('isabela.solorzano@example.com', 'happygoose177', 'thunder1', 'USER'),
+    ('owen.ruiz@example.com', 'bigmeercat968', 'barry1', 'USER'),
+    ('willard.bailey@example.com', 'happygorilla265', 'albatros', 'USER'),
+    ('lilou.arnaud@example.com', 'redmouse204', 'bologna', 'USER'),
+    ('hilda.olmos@example.com', 'orangegorilla488', 'precious', 'USER'),
+    ('adhira.uchil@example.com', 'brownbear655', 'arcadia', 'USER'),
+    ('julia.saez@example.com', 'heavyswan138', 'leeds', 'USER')
