334bfb4b9ec12c973009ee7fe28817428b35b420
Степанюк Антон <shamahamaa@gmail.com>
2023-06-17 21:23:15 +0300

    #26 create PollService

--- src/main/java/com/example/pollprojectmain/service/PollService.java
+++ src/main/java/com/example/pollprojectmain/service/PollService.java
@@ -1,5 +1,18 @@
 package com.example.pollprojectmain.service;
 
+import com.example.pollprojectmain.pojo.Response;
+import com.example.pollprojectmain.pojo.VoteRequest;
+import com.example.pollprojectmain.pojo.dto.PollDto;
+
+import java.util.List;
+
 public interface PollService {
+    public List<PollDto> getResultById(Integer id);
+    public PollDto getById(Integer id);
+    public List<PollDto> getByOwner(String username);
+    public List<PollDto> getAvailableFor(String username);
+    public PollDto create(PollDto poll);
+    public Response allowToVote(List<String> users);
+    public Response vote(VoteRequest voteRequest);
 
 }
