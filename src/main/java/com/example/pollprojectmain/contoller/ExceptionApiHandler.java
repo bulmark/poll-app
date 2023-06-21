4b0a6c1fb1f36b26db48e29cd8070d65949c27e1
Степанюк Антон <shamahamaa@gmail.com>
2023-06-21 12:07:42 +0300

    add exception handler

---
+++ src/main/java/com/example/pollprojectmain/contoller/ExceptionApiHandler.java
@@ -0,0 +1,28 @@
+package com.example.pollprojectmain.contoller;
+
+import com.example.pollprojectmain.exception.BadArgumentException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springdoc.api.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@RestControllerAdvice
+public class ExceptionApiHandler {
+
+    @ExceptionHandler(BadArgumentException.class)
+    public ResponseEntity<ErrorMessage> badArgumentException(BadArgumentException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorMessage(exception.getMessage()));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorMessage> entityNotFoundException(EntityNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorMessage(exception.getMessage()));
+    }
+
+}
