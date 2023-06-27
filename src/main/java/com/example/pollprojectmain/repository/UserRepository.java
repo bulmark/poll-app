88996599f07d6e7c6f0cacd44278d19571418808
Степанюк Антон <shamahamaa@gmail.com>
2023-06-27 23:31:17 +0300

    add impementation of UserDetails and UserDetailsService

--- src/main/java/com/example/pollprojectmain/repository/UserRepository.java
+++ src/main/java/com/example/pollprojectmain/repository/UserRepository.java
@@ -2,14 +2,13 @@ package com.example.pollprojectmain.repository;
 
 import com.example.pollprojectmain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findUserByUsername(String username);
+    Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
---
+++ src/main/java/com/example/pollprojectmain/service/Impl/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.demoauth.service;
+
+import com.example.pollprojectmain.model.User;
+import com.example.pollprojectmain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+        return com.example.demoauth.service.UserDetailsImpl.build(user);
+    }
+
+}
